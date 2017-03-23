e71f5b842b08c61a8e06cec98b0eca7d6b2bfb3e
johanlindholm <johan@lindholm.me>
2017-03-23 13:14:22 +0100

    Update ngcopy.js

--- ngcopy.js
+++ ngcopy.js
@@ -3,3 +3,17 @@ console.log("må du testes");
 
 
 alert("lol");
+
+
+// Anonymous "self-invoking" function
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+        // Use $ here...
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
