d0b87989ac1277e652d0a0490a1894116761f430
johanlindholm <johan@lindholm.me>
2017-03-23 13:20:09 +0100

    Update ngcopy2.js

--- ngcopy2.js
+++ ngcopy2.js
@@ -2,3 +2,20 @@ console.log("må du testes");
 text = window.getSelection().toString();
 console.log(text);
 
+function minFunction2() {
+    text = window.getSelection().toString();
+    var splitted = text.split('\n');
+    var line = "";
+    for (var i = 0; i < splitted.length; i++) {
+
+        delimited = splitted[i].split(',');
+
+        for (var j = 0; j < delimited.length; j++) {
+            //console.log(innerData[j]);
+            line += delimited[j] + "\t"
+        }
+        line += "\n"
+    }
+    //$('textarea#outputText').val(text);
+    alert(text)
+}
